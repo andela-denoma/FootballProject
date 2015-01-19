06a83b9f9c85700bd1ef32a500ef9c23ac312951
Enoma Deborah <deborah.enomah@andela.co>
2015-01-19 18:10:30 +0100

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+angular.module('football', ['ui.router', 'ngResource', 'footballController', 'footballServices'])
+.config(function ($stateProvider, $locationProvider){
+  $stateProvider
+  .state('forums', {
+    url: '/forums',
+    templateUrl: 'public/view/forums.html',
+    controller: 'ForumListCtrl'})
+  .state('newForum', {
+    url: '/forums/new',
+    templateUrl: 'public/view/forum-add.html',
+    controller: 'ForumCreateController'})
+  .state('viewForum', {
+    url: '/forums/:id/view',
+    templateUrl: 'public/view/forum-view.html',
+    controller: 'ForumViewController'})
+  .state('editForum', {
+    url: '/forums/:id/edit',
+    templateUrl: 'public/view/forum-edit.html',
+    controller: 'ForumEditController'})
+  .state('viewTopics', {
+    url: '/forums/:id/topics',
+    templateUrl: 'public/view/topics.html',
+    controller: 'TopicController'})
+  .state('newTopics', {
+    url: '/forums/:id/topics/add',
+    templateUrl: 'public/view/topic-add.html',
+    controller: 'TopicAddController'})
+  }).run (function ($state) {
+    $state.go('forums'); //make a transition to forums state when app starts
+  });
---
+++ public/js/controller.js
@@ -0,0 +1,99 @@
+angular.module('footballController', ['footballServices'])
+
+  .controller('ForumListCtrl', function ($scope, $state, $stateParams, $window, ForumService) {
+    $scope.getData = function (){
+      $scope.forums = ForumService.query();
+      console.log($scope.forums);
+    };
+    $scope.getData();
+  })
+
+  .controller('ForumViewController', function ($scope, $stateParams, ForumService, TopicService, CommentService){
+    $scope.forum = ForumService.get({ id: $stateParams.id});
+
+    $scope.comment = function(){
+      CommentService.get({id: $stateParams.id})
+    };
+    $scope.comment;
+
+  })
+
+  .controller('ForumCreateController', function ($scope, $state, $stateParams, ForumService) {
+    $scope.forum = new ForumService(); 
+    $scope.addForum = function() {
+      $scope.forum.$save(function(){
+        $state.go('forums');
+      });
+    };
+  })
+
+  .controller('ForumEditController', function ($scope, $state, $stateParams, ForumService) {
+    $scope.updateForum = function () {
+      $scope.forum.$update(function(){
+          $state.go('forums');
+      });
+    };
+    $scope.loadForum = function (){
+      $scope.forum = ForumService.get({id: $stateParams.id});
+    };
+    $scope.loadForum();
+  })
+
+  .controller('TopicController', function ($scope, $state, $stateParams, $window, TopicService, ForumService) {
+    $scope.topics = TopicService.query({id: $stateParams.id});
+    console.log($scope.topics);
+  })
+
+  .controller('TopicAddController', function ($scope, $state, $stateParams, $window, TopicService) {
+    $scope.topic = new TopicService(); 
+    $scope.addTopic = function() {
+      $scope.topic.$save(function(){
+        console.log($scope.topics);
+        $state.go('viewForums');
+      });
+    };
+  
+
+    $scope.deleteTopic = function(topic) {
+      forum.$delete(function() {
+        $window.location.href = ''; //redirect to home
+      });
+    };
+
+    $scope.updateTopic = function () {
+      $scope.topic.$update(function(){
+          $state.go('viewTopics');
+      });
+    };
+ })
+
+  .controller('CommentController', function ($scope, $state, $stateParams, $window, CommentService) {
+    $scope.comments = CommentService.query({id: $stateParams.id});
+    console.log($scope.comments);
+
+    $scope.comment = function(){
+      CommentService.get({id: $stateParams.id})
+    };
+
+    $scope.comment = new CommentService(); 
+    $scope.addComment = function() {
+      $scope.comment.$save(function(){
+        $window.location.href = ''
+      });
+    };
+
+    $scope.deleteComment = function(comment) {
+      comment.$delete(function() {
+        $window.location.href = ''; //redirect to home
+      });
+    };
+
+    $scope.updateComment = function () {
+      $scope.comment.$update(function(){
+          $state.go('viewTopics');
+      });
+    };
+  });
+  
+
+  
