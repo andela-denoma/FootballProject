126e44b47d75b6bf3f64b6c9e95684664924633f
Enoma Deborah <deborah.enomah@andela.co>
2015-01-19 18:32:31 +0100

    modified route

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 angular.module('football', ['ui.router', 'ngResource', 'footballController', 'footballServices'])
 .config(function ($stateProvider, $locationProvider){
+  $locationProvider.html5mode(true);
   $stateProvider
   .state('forums', {
     url: '/forums',
