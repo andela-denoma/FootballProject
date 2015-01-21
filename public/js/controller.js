81ae5de5d79e49b1eaaa593b4c7d9fa0214499aa
Enoma Deborah <deborah.enomah@andela.co>
2015-01-21 14:59:07 +0100

    modified

--- public/js/controller.js
+++ public/js/controller.js
@@ -8,19 +8,9 @@ angular.module('footballController', ['footballServices'])
     $scope.getData();
   })
 
-  .controller('ForumViewController', function ($scope, $stateParams, ForumService, TopicService, CommentService){
-    $scope.forum = ForumService.get({ id: $stateParams.id});
-    $scope.topics = TopicService.query({id: $stateParams.id});
-    $scope.comments = CommentService.query({id: $stateParams.id});
-
-    $scope.topic = new TopicService(); 
-    $scope.addTopic = function() {
-      $scope.topic.$save(function(){
-        console.log($scope.topics);
-        $state.go('viewForums');
-      });
-    };
-    
+  .controller('ForumViewController', function ($scope, $stateParams, ForumService){
+    $scope.forum = ForumService.get({ name: $stateParams.name});
+    console.log($scope.forum);
   })
 
   .controller('ForumCreateController', function ($scope, $state, $stateParams, ForumService) {
@@ -38,61 +28,10 @@ angular.module('footballController', ['footballServices'])
           $state.go('forums');
       });
     };
+  
     $scope.loadForum = function (){
       $scope.forum = ForumService.get({id: $stateParams.id});
     };
     $scope.loadForum();
-  })
-
-  .controller('TopicController', function ($scope, $state, $stateParams, $window, TopicService, ForumService) {
-    $scope.topics = TopicService.query({id: $stateParams.id});
-    console.log($scope.topics);
-  })
-
-  .controller('TopicAddController', function ($scope, $state, $stateParams, $window, TopicService) {
-   
-  
-
-    $scope.deleteTopic = function(topic) {
-      forum.$delete(function() {
-        $window.location.href = ''; //redirect to home
-      });
-    };
-
-    $scope.updateTopic = function () {
-      $scope.topic.$update(function(){
-          $state.go('viewTopics');
-      });
-    };
- })
-
-  .controller('CommentController', function ($scope, $state, $stateParams, $window, CommentService) {
-    $scope.comments = CommentService.query({id: $stateParams.id});
-    console.log($scope.comments);
-
-    $scope.comment = function(){
-      CommentService.get({id: $stateParams.id})
-    };
-
-    $scope.comment = new CommentService(); 
-    $scope.addComment = function() {
-      $scope.comment.$save(function(){
-        $window.location.href = ''
-      });
-    };
-
-    $scope.deleteComment = function(comment) {
-      comment.$delete(function() {
-        $window.location.href = ''; //redirect to home
-      });
-    };
-
-    $scope.updateComment = function () {
-      $scope.comment.$update(function(){
-          $state.go('viewTopics');
-      });
-    };
   });
-  
 
-  
